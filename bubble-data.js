e162fd7033db19ae7df3a7578bc03ba2ab27e538
prajeet21 <prajeetbhavsar97@gmail.com>
2020-04-18 14:14:22 -0700

    bubble chart demo

---
+++ bubble-data.js
@@ -0,0 +1,179 @@
+const dataset = {
+    "children": [{"Name":"Olives","Count":4319},
+        {"Name":"Tea","Count":4159},
+        {"Name":"Mashed Potatoes","Count":2583},
+        {"Name":"Boiled Potatoes","Count":2074},
+        {"Name":"Milk","Count":1894},
+        {"Name":"Chicken Salad","Count":1809},
+        {"Name":"Vanilla Ice Cream","Count":1713},
+        {"Name":"Cocoa","Count":1636},
+        {"Name":"Lettuce Salad","Count":1566},
+        {"Name":"Lobster Salad","Count":1511},
+        {"Name":"Chocolate","Count":1489},
+        {"Name":"Apple Pie","Count":1487},
+        {"Name":"Orange Juice","Count":1423},
+        {"Name":"American Cheese","Count":1372},
+        {"Name":"Green Peas","Count":1341},
+        {"Name":"Assorted Cakes","Count":1331},
+        {"Name":"French Fried Potatoes","Count":1328},
+        {"Name":"Potato Salad","Count":1306},
+        {"Name":"Baked Potatoes","Count":1293},
+        {"Name":"Roquefort","Count":1273},
+        {"Name":"Stewed Prunes","Count":1268}]
+};
+
+const yelpData = {
+"Sandwiches": {
+"count": 418
+},
+"Irish": {
+"count": 5
+},
+"Mexican": {
+"count": 512
+},
+"Indian": {
+"count": 39
+},
+"Cafes": {
+"count": 143
+},
+"American": {
+"count": 426
+},
+"Burgers": {
+"count": 180
+},
+"Coffee N Tea": {
+"count": 50
+},
+"Chinese": {
+"count": 171
+},
+"Italian": {
+"count": 232
+},
+"Juice Bars": {
+"count": 19
+},
+"Mediterranean": {
+"count": 99
+},
+"Sports Bar": {
+"count": 127
+},
+"Fast Food": {
+"count": 70
+},
+"Japanese": {
+"count": 92
+},
+"Breakfast N Brunch": {
+"count": 43
+},
+"Vegan": {
+"count": 23
+},
+"Bar": {
+"count": 2
+},
+"British": {
+"count": 6
+},
+"Thai": {
+"count": 37
+},
+"Barbeque": {
+"count": 31
+},
+"Vietnamese": {
+"count": 31
+},
+"Deli": {
+"count": 8
+},
+"Diner": {
+"count": 2
+},
+"Steakhouses": {
+"count": 7
+},
+"Middle Eastern": {
+"count": 5
+},
+"Greek": {
+"count": 9
+},
+"Seafood": {
+"count": 47
+},
+"Frozen Yogurt": {
+"count": 5
+},
+"Bakery": {
+"count": 8
+},
+"Kosher": {
+"count": 2
+},
+"Hawaiian": {
+"count": 10
+},
+"Korean": {
+"count": 3
+},
+"Buffet": {
+"count": 2
+},
+"Ethiopian": {
+"count": 2
+},
+"Czech": {
+"count": 1
+},
+"Southern": {
+"count": 5
+},
+"European": {
+"count": 3
+},
+"Fish N Chips": {
+"count": 2
+},
+"Donuts": {
+"count": 2
+},
+"Asian Fusion": {
+"count": 5
+},
+"Vegetarian": {
+"count": 5
+},
+"Cambodian": {
+"count": 1
+},
+"Caribbean": {
+"count": 3
+},
+"Peruvian": {
+"count": 1
+},
+"Filipino": {
+"count": 2
+},
+"Food Truck": {
+"count": 1
+},
+"Brewery": {
+"count": 2
+},
+"Tea Room": {
+"count": 1
+},
+"French": {
+"count": 2
+},
+"Lounge": {
+"count": 1
+}
+};
